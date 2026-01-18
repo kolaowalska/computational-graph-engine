2e45e35ad20e5bad30cf32c8038f5018678fec9b
KOLA <normalnie.kowalska@gmail.com>
2026-01-18 20:16:54 +0100

    environment setup

---
+++ cmake-build-debug/CMakeFiles/cg.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for cg.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/computational-graph-engine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/computational-graph-engine.dir/link.d"
+  "CMakeFiles/computational-graph-engine.dir/src/main.cpp.o"
+  "CMakeFiles/computational-graph-engine.dir/src/main.cpp.o.d"
+  "computational-graph-engine"
+  "computational-graph-engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/computational-graph-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
