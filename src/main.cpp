0f2f6e326ef5e290ce62d15831554f17390a5a30
KOLA <normalnie.kowalska@gmail.com>
2026-01-20 14:13:35 +0100

    diagnosed shit code

--- src/main.cpp
+++ src/main.cpp
@@ -8,4 +8,22 @@ int main() {
 
     cg::Graph<T> G;
 
+    // f(x, y) = sin(x) * (y + 2) + 3 * x * x
+    auto x = cg::input(G, "x");
+    auto y = cg::input(G, "y");
+
+    // TODO: fix this
+    // auto expr = cg::sin(x) * (y + cg::constant(G, 2.0))
+    //           + cg::constant(G, 3.0) * x * x;
+
+    cg::Context<T> ctx;
+
+    std::cout << "x = "; std::cin >> ctx["x"];
+    std::cout << "y = "; std::cin >> ctx["y"];
+
+    cg::Evaluator<T, cg::eval::NaiveEvaluator> eval;
+    // auto result = eval.evaluate(G, expr.root(), ctx);
+
+    // std::cout << "result = " << result << "\n";
+    std::cout << "nodes  = " << G.size() << "\n";
 }
