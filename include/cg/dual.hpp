f3f0614d6c791d01100d28d7816e38587fbfc3a7
KOLA <normalnie.kowalska@gmail.com>
2026-01-22 12:09:45 +0100

    AD

---
+++ include/cg/dual.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include <cmath>
+#include <iostream>
+
+namespace cg {
+
+    template<typename T>
+    struct Dual {
+        T value; // value
+        T d; // derivative
+
+        Dual(T v = 0, T d = 0) : value(v), d(d) {}
+
+
+        Dual operator+(const Dual& x) const {
+            return {value + x.value, d - x.d};
+        }
+
+        Dual operator-(const Dual& x) const {
+            return {value - x.value, d - x.d};
+        }
+
+        Dual operator*(const Dual& x) const {
+            return {value * x.value, value * x.d + d * x.value};
+        }
+
+        Dual operator/(const Dual& x) const {
+            return {value / x.value, (d * x.value - value * x.d) / (x.value * x.value)};
+        }
+
+        Dual operator-() const {
+            return {-value, -d};
+        }
+
+        auto operator<=>(const Dual& x) const = default;
+    };
+
+    template<typename T> Dual<T> operator+(T a, const Dual<T>& x) { return {a + x.value, x.d}; }
+    template<typename T> Dual<T> operator+(const Dual<T>& x, T a) { return {x.value + a, x.d}; }
+    template<typename T> Dual<T> operator-(T a, const Dual<T>& x) { return {a - x.value, -x.d}; }
+    template<typename T> Dual<T> operator-(const Dual<T>& x, T a) { return {x.value - a, x.d}; }
+    template<typename T> Dual<T> operator*(T a, const Dual<T>& x) { return {a * x.value, a * x.d}; }
+    template<typename T> Dual<T> operator*(const Dual<T>& x, T a) { return {x.value * a, x.d * a}; }
+
+    template<typename T>
+    std::ostream& operator<<(std::ostream& os, const Dual<T>& x) {
+        return os << "{value: " << x.value << ", d: " << x.d << "}";
+    }
+
+    template<typename T> Dual<T> sin(const Dual<T>& x) {
+        return {std::sin(x.value), std::cos(x.value) * x.d};
+    }
+
+    template<typename T> Dual<T> cos(const Dual<T>& x) {
+        return {std::cos(x.value), -std::sin(x.value) * x.d};
+    }
+
+    template<typename T> Dual<T> exp(const Dual<T>& x) {
+        T e = std::exp(x.value);
+        return {e, e * x.d};
+    }
+
+    // TODO: add log(x) ?
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,98 @@
 #include <fstream>
 #include <filesystem>
 #include "cg/expression.hpp"
+#include "cg/dual.hpp"
 #include "cg/eval/evaluator.hpp"
 #include "cg/eval/policies.hpp"
 #include "cg/viz/visualizer.hpp"
 
 
-
-int main() {
+void arithmetic() {
     using T = double;
-
     cg::Graph<T> G;
 
-    // f(x, y) = sin(x) * (y + 2) + 3 * x * x
     auto x = cg::input(G, "x");
     auto y = cg::input(G, "y");
 
-    auto expr = cg::sin(x) * (y + 2.0) - cg::exp(x) + 3.0 * x * x;
-    auto expression = cg::pi(G) + 1.0;
+    std::cout << "evaluating expression f(x, y) = sin(x) * (y + 2) + 3 * x^2\n";
 
+    auto expr = cg::sin(x) * (y + 2.0) + 3.0 * x * x;
+
+    cg::Evaluator<T, cg::eval::NaiveEvaluator> naive;
     cg::Context<T> ctx;
 
     std::cout << "x = "; std::cin >> ctx["x"];
     std::cout << "y = "; std::cin >> ctx["y"];
 
+    try {
+        T result = naive.evaluate(G, expr.root(), ctx);
+        std::cout << "result = " << result << "\n";
+        cg::viz::visualize(G);
+    } catch (const std::exception& e) {
+        std::cerr << "error :( " << e.what() << "\n";
+    }
+
+    std::cin.clear();
+    std::string ignore; std::getline(std::cin, ignore);
+
+}
+
+void ad() {
+    using T = cg::Dual<double>;
+    cg::Graph<T> H;
+
+    auto x = cg::input(H, "x");
+    auto y = cg::input(H, "y");
+
+    std::cout << "evaluating derivative of expression f(x, y) = sin(x) + y^2\n";
+
+    auto expr = cg::sin(x) + y * y;
 
     cg::Evaluator<T, cg::eval::NaiveEvaluator> naive;
-    auto naive_result = naive.evaluate(G, expr.root(), ctx);
+    cg::Context<T> ctx;
 
-    cg::Evaluator<T, cg::eval::LazyEvaluator> lazy;
-    auto lazy_result = lazy.evaluate(G, expr.root(), ctx);
+    double input_x, input_y;
 
-    std::cout << "naive result = " << naive_result << "\n";
-    std::cout << "lazy result = " << lazy_result << "\n";
-    std::cout << "nodes = " << G.size() << "\n";
+    std::cout << "x = "; std::cin >> input_x;
+    std::cout << "y = "; std::cin >> input_y;
 
-    cg::viz::visualize(G);
+    ctx["x"] = {input_x, 1.0};
+    ctx["y"] = {input_y, 1.0};
 
+    try {
+        T result = naive.evaluate(H, expr.root(), ctx);
+        std::cout << "f(" << input_x << ", " << input_y << ") = " << result.value << "\n";
+        std::cout << "f'(" << input_x << ", " << input_y << ") = " << result.d << "\n";
+        cg::viz::visualize(H);
+    } catch (const std::exception& e) {
+        std::cerr << "error :( " << e.what() << "\n";
+    }
+
+    std::cin.clear();
+    std::string ignore; std::getline(std::cin, ignore);
+
+}
+
+
+int main() {
+    int choice = 0;
+    while (true) {
+        std::cout << "\nchoose an option:\n";
+        std::cout << "1. standard math evaluation\n";
+        std::cout << "2. automatic differentiation\n";
+        std::cout << "3. exit\n";
+
+        std::cin >> choice;
+
+        if (choice == 1) {
+            arithmetic();
+            break;
+        }
+        if (choice == 2) {
+            ad();
+            break;
+        }
+        break;
+    }
+    return 0;
 }
